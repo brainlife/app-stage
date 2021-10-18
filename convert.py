7b8f3be14f1cf471a953da39b4ea28d90c569fd0
Soichi Hayashi <hayashis@iu.edu>
2021-10-18 13:25:59 +0000

    added -b y optino for dcm2niix

--- convert.py
+++ convert.py
@@ -45,16 +45,23 @@ with open("config.json") as config_json:
                 name="dwi"
 
             print("unzipping")
-            code=subprocess.call(["unzip", "-o", "-d", "dicom", "dicom.zip"], cwd=outdir)
-            if code != 0: 
+            ret=subprocess.run(["unzip", "-o", "-d", "dicom", "dicom.zip"], cwd=outdir)
+            if ret.returncode != 0: 
                 print("failed to unzip")
-                sys.exit(code)            
+                sys.exit(ret.returncode)            
 
             print("running dcm2niix on %s" % outdir)
-            code=subprocess.call(["dcm2niix", "-v", "1", "-z", "o", "-d", "10", "-w", "1", "-f", name, "."], cwd=outdir)
-            if code != 0: 
+            #-v : verbose (n/y or 0/1/2, default 0) [no, yes, logorrheic
+            #-z : gz compress images (y/o/i/n/3, default n) [y=pigz, o=optimal pigz, i=internal:miniz, n=no, 3=no,3D]
+            #-d : directory search depth. Convert DICOMs in sub-folders of in_folder? (0..9, default 5
+            #-w : write behavior for name conflicts (0,1,2, default 2: 0=skip duplicates, 1=overwrite, 2=add suffix)
+            #-b : BIDS sidecar (y/n/o [o=only: no NIfTI], default y)
+            #-f : filename 
+            #. < input folder
+            ret=subprocess.run(["dcm2niix", "-v", "1", "-z", "o", "-d", "10", "-w", "1", "-b", "y", "-f", name, "."], cwd=outdir)
+            if ret.returncode != 0: 
                 print("failed to run dcm2niix")
-                sys.exit(code)            
+                sys.exit(ret.returncode)            
 
             print("loading sidecar .json and creating product.json")
             sidecar_jsons = glob.glob(outdir+"/*.json")
